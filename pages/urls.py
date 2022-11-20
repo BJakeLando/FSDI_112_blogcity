de309bbd4f1b4e508b6ef8fe3a2b581c81c00be4
Brandon Landers <bjlando@gmail.com>
2022-11-20 12:43:15 -0800

    Assignment 3

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import (
+            AboutView,
+            HomeView
+            )
+
+urlpatterns = [
+    path('about/', AboutView.as_view(), name= 'about'),
+    path('', HomeView.as_view(), name= 'home'),
+]
